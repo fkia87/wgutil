0c1dd3b88a0e994f2586ce9d5454afa56ac054a0
fkia <fkia@protonmail.com>
2023-09-04 22:08:54 +0330

    update

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,9 +2,6 @@
 # shellcheck disable=SC2068
 set -euo pipefail
 
-resources_latest_version=$(curl -v https://github.com/fkia87/resources/releases/latest 2>&1 | \
-    grep -i location | rev | cut -d / -f 1 | rev | sed 's/\r//g' | cat -v)
-
 download_resources() {
     echo -e "Downloading resources..."
     rm -rf "$resources_latest_version".tar.gz
@@ -17,6 +14,11 @@ cleanup() {
     rm -rf ./resources*
 }
 
+#################################################################################
+
+resources_latest_version=$(curl -v https://github.com/fkia87/resources/releases/latest 2>&1 | \
+    grep -i location | rev | cut -d / -f 1 | rev | sed 's/\r//g' | cat -v)
+
 download_resources
 
 requirements=(
@@ -26,9 +28,13 @@ requirements=(
     )
 
 for file in ${requirements[@]}; do
-    install -m 644 "$file" "/etc/profile.d/${file##*/}.sh"
+    install -m 644 "$file" "/etc/profile.d/${file##*/}.sh" && \
+        echo -e "Installed: /etc/profile.d/${file##*/}.sh"
 done
 
 cleanup
 
-install -m 755 ./wgutil /usr/bin && echo "Installation done."
+install -m 755 ./wgutil /usr/bin/wgutil && \
+    { echo -e "Installed: /usr/bin/wgutil\nInstallation done."; exit 0; }
+
+exit 1
