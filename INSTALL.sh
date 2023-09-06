3ba1b5b803f104e6cc9419103b6809a17cd1b279
fkia <fkia@protonmail.com>
2023-09-06 19:38:40 +0330

    update

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,9 +2,12 @@
 # shellcheck disable=SC2068
 set -euo pipefail
 
-[[ $UID == "0" ]] || { echo "You are not root." >&2; exit 1; }
-
 install_resources() {
+    [[ $UID == "0" ]] || { echo "You are not root." >&2; exit 1; }
+    resources_latest_version=$(
+        curl -v https://github.com/fkia87/resources/releases/latest 2>&1 | \
+        grep -i location | rev | cut -d / -f 1 | rev | sed 's/\r//g'
+    )
     echo -e "Downloading resources..."
     rm -rf "$resources_latest_version".tar.gz
     wget https://github.com/fkia87/resources/archive/refs/tags/"$resources_latest_version".tar.gz || \
@@ -17,11 +20,6 @@ install_resources() {
 }
 
 #################################################################################
-
-resources_latest_version=$(
-    curl -v https://github.com/fkia87/resources/releases/latest 2>&1 | \
-    grep -i location | rev | cut -d / -f 1 | rev | sed 's/\r//g'
-    )
 
 install_resources
 
