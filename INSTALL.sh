3a3eb64254abaeadcbf6b823a910b7ec0c4ed98b
fkia <fkia@protonmail.com>
2026-02-06 23:13:34 +0330

    update

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,13 +5,14 @@
 
 # comp_location='/etc/bash_completion.d'
 comp_location=/usr/share/bash-completion/completions
-
+comp_script=./wgutil-completion.bash
 
 install -v -m 755 ./wgutil /usr/bin/wgutil && \
     echo -e "\nInstallation Successfull!\n"
 mkdir -p "$comp_location"
-install -v -m 644 ./wgutil-completion.bash "$comp_location"/wgutil && \
+install -v -m 644 $comp_script "$comp_location"/wgutil && \
     {
         echo -e "\nPlease run this command to activate \"wgutil\" auto completion:\n";
         echo -e "source ${comp_location}/wgutil\n";
+        echo -e "Or simply restart your shell.";
     }
